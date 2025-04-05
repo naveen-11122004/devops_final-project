d39256ca183b45827a41a431ce40c44428a96d73
naveen-11122004 <105262683+naveen-11122004@users.noreply.github.com>
2025-04-05 09:40:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build -t task_final .
 echo Hyyy
-docker login -u boopeshs -p 02-Apr-05
-docker tag task_final boopeshs/devops_final
-docker push boopeshs/devops_final
+docker login -u naveen11122004 -p naveen2004
+docker tag task_final naveen11122004/devops_final
+docker push naveen11122004/devops_final
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
